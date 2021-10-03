196148cb8fbdcbc24213b903f9e2fbe65aee58a9
aymensp <aymen.smati@esprit.tn>
2021-10-03 23:46:06 +0100

    working on episod Details

--- components/EpisodItem.tsx
+++ components/EpisodItem.tsx
@@ -1,16 +1,79 @@
+import { AntDesign } from '@expo/vector-icons'
 import React from 'react'
-import { StyleSheet, Image, View } from 'react-native'
+import { StyleSheet, Image, View, Text } from 'react-native'
 
-const EpisodItem = () => {
+interface EpisodeProps {
+    episode: {
+        id: string,
+        title: string,
+        poster: string,
+        duration:string,
+        plot:string,
+        video:string
+    }
+}
+
+const EpisodItem = (props: EpisodeProps) => {
+    const {episode} =props;
     return (
-        <View>
-           <View>
-               
-           </View>
+        <View style={{marginTop:20}}>
+            <View style={styles.EpisodContainer}>
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'space-between'
+                    }}
+                >
+                    <Image
+                        style={styles.EpisodeImage}
+                        source={{
+                            uri: episode.poster
+                        }}
+                    />
+                    <View>
+                        <Text style={styles.EpisodTitle}>{episode.title}</Text>
+                        <Text style={styles.EpisodTime}>1h 21m</Text>
+
+                    </View>
+                </View>
+                <AntDesign name='download' size={24} color='white' />
+            </View>
+            <Text style={styles.EpisodPlot}>
+                An open-and-shut case becomes anything but when Harvey is accused of an inappropriate dalliance with a married woman.
+            </Text>
         </View>
     )
 }
 
 export default EpisodItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    EpisodContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 10
+    },
+    EpisodeImage: {
+        height: 75,
+        width: 130,
+        borderRadius: 5,
+        marginRight: 10
+    },
+    EpisodTitle: {
+        color: 'white',
+        fontWeight: '400',
+        fontSize: 14
+    },
+    EpisodTime: {
+        color: 'gray',
+        fontWeight: '400',
+        fontSize: 11
+    },
+    EpisodPlot: {
+        fontSize: 15,
+        color: '#bdc0bd',
+        fontWeight: '400'
+    }
+})
--- screens/MovieDetailScreen.tsx
+++ screens/MovieDetailScreen.tsx
@@ -1,6 +1,6 @@
 import { AntDesign, Feather, FontAwesome, Ionicons } from '@expo/vector-icons'
 import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import { Button } from 'react-native-elements'
 import movie from '../assets/data/movie'
 import EpisodItem from '../components/EpisodItem'
@@ -14,57 +14,70 @@ const MovieDetailScreen = () => {
                     uri: movie.seasons.items[0].episodes.items[0].poster
                 }}
             />
-            <View style={styles.movieDetail}>
-                <Text style={styles.title}>Suits</Text>
-                <View style={styles.subtitle}>
-                    <Text style={{ color: '#05c405', fontSize: 15, fontWeight: '500' }}>98% match </Text>
-                    <Text style={{ color: 'white', fontSize: 15, fontWeight: '500' }}> {movie.year} </Text>
-                    <View style={styles.ageContainer}>
-                        <Text style={styles.ageText}>16+</Text>
+
+            <FlatList
+                data={movie.seasons.items[0].episodes.items}
+                renderItem={({ item }) => (
+                    <EpisodItem episode={item} />
+                )}
+                style={{ marginBottom: 250 }}
+                ListHeaderComponent={(
+                    <View style={styles.movieDetail}>
+                        <Text style={styles.title}>Suits</Text>
+                        <View style={styles.subtitle}>
+                            <Text style={{ color: '#05c405', fontSize: 15, fontWeight: '500' }}>98% match </Text>
+                            <Text style={{ color: 'white', fontSize: 15, fontWeight: '500' }}> {movie.year} </Text>
+                            <View style={styles.ageContainer}>
+                                <Text style={styles.ageText}>16+</Text>
+                            </View>
+                            <Text style={{ color: 'white', fontSize: 15, fontWeight: '500' }}>{movie.numberOfSeasons} seasons</Text>
+                            <Text style={styles.HdText}>HD</Text>
+                        </View>
+                        <Button
+                            title="Play"
+                            icon={<Ionicons name='play' size={15} color='black' />}
+                            titleStyle={{ color: 'black', fontSize: 16, fontWeight: 'bold', marginLeft: 8 }}
+                            buttonStyle={styles.button}
+                        />
+
+                        <Button
+                            title="Download"
+                            icon={<AntDesign name='download' size={15} color='white' />}
+                            titleStyle={{ color: 'white', fontSize: 16, fontWeight: 'bold', marginLeft: 8 }}
+                            buttonStyle={styles.buttonDownload}
+                        />
+                        <Text style={{ color: 'white', fontSize: 15, fontWeight: '500', marginBottom: 12 }}>{movie.plot}</Text>
+                        <Text style={{ color: 'gray', fontSize: 12, fontWeight: '500', marginBottom: 3 }}>Cast: {movie.cast}</Text>
+                        <Text style={{ color: 'gray', fontSize: 12, fontWeight: '500' }}>Creator: {movie.creator}</Text>
+                        <View style={styles.butons}>
+                            <Button icon={<Ionicons name='add' size={28} color='white' />}
+                                title='My list'
+                                iconPosition='top'
+                                titleStyle={{ fontWeight: "500", fontSize: 11, color: 'gray' }}
+                                buttonStyle={{ backgroundColor: 'transparent' }}
+                            />
+                            <Button icon={<Feather name='thumbs-up' size={25} color='white' />}
+                                title='Like'
+                                iconPosition='top'
+                                titleStyle={{ fontWeight: "500", fontSize: 11, color: 'gray', marginTop: 6 }}
+                                buttonStyle={{ backgroundColor: 'transparent' }}
+                            />
+                            <Button icon={<FontAwesome name="send-o" size={22} color='white' />}
+                                title='Share'
+                                iconPosition='top'
+                                titleStyle={{ fontWeight: "500", fontSize: 11, color: 'gray', marginTop: 9 }}
+                                buttonStyle={{ backgroundColor: 'transparent' }}
+                            />
+                        </View>
+                        <View style={{ width: "15%", backgroundColor: 'red', height: 5 }} />
+
+
+
                     </View>
-                    <Text style={{ color: 'white', fontSize: 15, fontWeight: '500' }}>{movie.numberOfSeasons} seasons</Text>
-                    <Text style={styles.HdText}>HD</Text>
-                </View>
-                <Button
-                    title="Play"
-                    icon={<Ionicons name='play' size={15} color='black' />}
-                    titleStyle={{ color: 'black', fontSize: 16, fontWeight: 'bold', marginLeft: 8 }}
-                    buttonStyle={styles.button}
-                />
-                <Button
-                    title="Download"
-                    icon={<AntDesign name='download' size={15} color='white' />}
-                    titleStyle={{ color: 'white', fontSize: 16, fontWeight: 'bold', marginLeft: 8 }}
-                    buttonStyle={styles.buttonDownload}
-                />
-                <Text style={{ color: 'white', fontSize: 15, fontWeight: '500', marginBottom: 12 }}>{movie.plot}</Text>
-                <Text style={{ color: 'gray', fontSize: 12, fontWeight: '500', marginBottom: 3 }}>Cast: {movie.cast}</Text>
-                <Text style={{ color: 'gray', fontSize: 12, fontWeight: '500' }}>Creator: {movie.creator}</Text>
-                <View style={styles.butons}>
-                    <Button icon={<Ionicons name='add' size={28} color='white' />}
-                        title='My list'
-                        iconPosition='top'
-                        titleStyle={{ fontWeight: "500", fontSize: 11, color: 'gray' }}
-                        buttonStyle={{ backgroundColor: 'transparent' }}
-                    />
-                    <Button icon={<Feather name='thumbs-up' size={25} color='white' />}
-                        title='Like'
-                        iconPosition='top'
-                        titleStyle={{ fontWeight: "500", fontSize: 11, color: 'gray', marginTop: 6 }}
-                        buttonStyle={{ backgroundColor: 'transparent' }}
-                    />
-                    <Button icon={<FontAwesome name="send-o" size={22} color='white' />}
-                        title='Share'
-                        iconPosition='top'
-                        titleStyle={{ fontWeight: "500", fontSize: 11, color: 'gray', marginTop: 9 }}
-                        buttonStyle={{ backgroundColor: 'transparent' }}
-                    />
-                </View>
-                <View style={{ width: "15%", backgroundColor: 'red', height: 5 }} />
-                <EpisodItem/>
-            </View>
-            
-           
+
+                )}
+            />
+
         </View>
     )
 }
